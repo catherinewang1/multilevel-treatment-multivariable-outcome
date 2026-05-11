cb8b47947bdd3696d6533ae3f67c9e9570e13b8d
Catherine Wang <catheri2@andrew.cmu.edu>
2026-05-11 19:55:32 -0400

    CW: real EBCI replogle- add script to copy over the shrinkage all.png and all.pdf files into a separate folder to more easily transfer just these files

---
+++ notebooks/realEBCICell/5_copy_shrinkageallplots.r
@@ -0,0 +1,42 @@
+# done on server bc windows limits path file length....
+# copy over the all.pdf and all.png pics over from previously saved EBCI/shrinkage/ folder
+# this is to more easily copy just these plots to overleaf, without uploading too many large things
+plot_path         = '../../plots/replogle/EBCI/'    # location to save plots of EBCI analysis on replogle dataset
+
+dir.create(sprintf('%s/final/', plot_path))
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////
+# ================================== select only some prev saved files =============================================
+# //////////////////////////////////////////////////////////////////////////////////////////////////
+# copy over the all.pdf and all.png pics over from previously saved EBCI/shrinkage/ folder
+# this is to more easily copy just these plots to overleaf, without uploading too many large things
+assertthat::assert_that(dir.exists(sprintf('%s/final/', plot_path)))
+dir.create(sprintf('%s/final/selAllPlots/', plot_path))
+
+
+
+
+files_to_copy = list.files(path = sprintf('%s/shrinkage/', plot_path) , 
+                           pattern = 'all', recursive=TRUE, full.names = TRUE)
+dir_to_create = sub(pattern = sprintf('%s/shrinkage/', plot_path), replacement = '', x = files_to_copy) # just the folder names
+dir_to_create = sub(pattern = 'all.((png)|(pdf))', replacement = '', x = dir_to_create) # just the folder names
+
+for(i in 1:length(files_to_copy)) {
+  cur_file   = files_to_copy[i]
+  cur_folder = dir_to_create[i]
+  print(sprintf('%s/final/selAllPlots/%s', plot_path, cur_folder))
+  dir.create(sprintf('%s/final/selAllPlots/%s', plot_path, cur_folder), recursive=TRUE)
+  
+  file.copy(from = cur_file, 
+            to = sprintf('%s/final/selAllPlots/%s/%s', 
+                         plot_path, cur_folder, 
+                         substr(cur_file, nchar(cur_file)-6, nchar(cur_file))), 
+            overwrite = TRUE)
+  
+  
+}
+
+
+
+
+
